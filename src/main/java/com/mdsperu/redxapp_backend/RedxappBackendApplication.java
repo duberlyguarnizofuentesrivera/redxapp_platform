14a6bd4986a7c7f71e828ae5b2bcd385a2b3c111
Duberly Guarnizo <duberlygfr@gmail.com>
2022-08-10 03:07:48 -0500

    Initial commit

---
+++ src/main/java/com/mdsperu/redxapp_backend/RedxappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mdsperu.redxapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedxappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedxappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdsperu/redxapp_backend/RedxappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsperu.redxapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedxappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
